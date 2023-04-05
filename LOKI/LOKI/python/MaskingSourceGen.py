aa7777d57a51028ec5615a0e8f24a77818587ff5
MilanKlausz <Milan.Klausz@ess.eu>
2023-04-05 13:56:41 +0200

    added option to aim at a certain bank for masking

--- LOKI/LOKI/python/MaskingSourceGen.py
+++ LOKI/LOKI/python/MaskingSourceGen.py
@@ -11,17 +11,32 @@ class MaskingSourceGen(G4CustomPyGen.GenBase):
         self.addParameterDouble("gen_x_width_meters", 0.0)
         self.addParameterDouble("gen_y_width_meters", 0.0)
         #self.addParameterDouble("z_width_meters", 0.001)
-        self.addParameterInt("aiming_pixel_id_min", 0)
+        self.addParameterInt("aiming_bank_id", 0, 0, 8)
+        self.addParameterInt("aiming_pixel_id_min", -1)
         self.addParameterInt("aiming_straw_pixel_number", 512) #used only for aiming, NOT for analysis
 
     def init_generator(self,gun):
-        gun.set_type('geantino') #neutron
-
-        self._i = self.aiming_pixel_id_min -1 #count events to shoot neutrons at each pixel
+        gun.set_type('geantino')
 
         self.aimHelper = Mask.MaskingHelper(self.geo_rear_detector_distance_m *Units.m, self.aiming_straw_pixel_number)
         self.totalNumberOfPixels = self.aimHelper.getTotalNumberOfPixels()
-        print(f"Total number of pixels: {self.totalNumberOfPixels}")
+        bank_pixel_id_min = self.aimHelper.getBankPixelOffset(self.aiming_bank_id)
+        number_of_pixels_in_bank = self.aimHelper.getNumberOfPixels(self.aiming_bank_id)
+
+        print(f"Total number of pixels in full geometry: {self.totalNumberOfPixels} (set -n accordingly to cover the whole geometry!)")
+        print(f"Number of pixels in bank{self.aiming_bank_id}: {number_of_pixels_in_bank} (set -n accordingly to cover the whole bank!)")
+        print(f"Lowest pixel id in bank{self.aiming_bank_id}: {bank_pixel_id_min}")
+
+        self._i = bank_pixel_id_min  #count events to shoot neutrons at each pixel
+        if(self.aiming_pixel_id_min>=0):
+          self._i = self.aiming_pixel_id_min
+          print(f"aiming_pixel_id_min is set to: {self.aiming_pixel_id_min}. This will be used as the first pixel to aim at, overriding the lowest pixel id in the selected/default bank.")
+        else:
+          print(f"First pixel to aim at: {self._i}")
+        self._i -= 1 # incrementation happens at the begining of the generate_event loop
+
+        #bank pixel limits: 0, 401408, 516096, 602112, 716800, 802816, 1003520, 1232896, 1376256, 1605632 (for 256 pixels/straw)
+        #                   0, 802816, 1032192, 1204224, 1433600, 1605632, 2007040, 2465792, 2752512, 3211264 (for 512 pixels/straw)
 
     def generate_event(self,gun):
         self._i += 1
@@ -32,7 +47,6 @@ class MaskingSourceGen(G4CustomPyGen.GenBase):
         gun.set_position(sourcePositionX, sourcePositionY, sourcePositionZ)
 
         # Direction - toward the centre of a pixel
-        #bank pixel limits: 0, 401408, 516096, 602112, 716800, 802816, 1003520, 1232896, 1376256, 1605632
         pixelId = ((self._i + 0) % self.totalNumberOfPixels)
 
         pixelCentreX, pixelCentreY, pixelCentreZ = self.aimHelper.getPixelCentrePositionsForMasking(pixelId, self.geo_old_tube_numbering, self.geo_larmor_2022_experiment)
